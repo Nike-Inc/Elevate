4f23811cc4286bcd69e732631658ff595bdd24c6
Eric Appel <eric.appel@nike.com>
2015-07-14 17:51:44 -0700

    reorganizing tests and adding BaseTestCase

---
+++ Tests/DecodableTests.swift
@@ -0,0 +1,12 @@
+//
+//  ElevateDecodableTests.swift
+//  Elevate
+//
+//  Created by Eric Appel on 7/14/15.
+//  Copyright © 2015 Nike. All rights reserved.
+//
+
+import Foundation
+import XCTest
+import Elevate
+
---
+++ Tests/ParserTests.swift
@@ -0,0 +1,11 @@
+//
+//  ParserTests.swift
+//  Elevate
+//
+//  Created by Eric Appel on 7/14/15.
+//  Copyright © 2015 Nike. All rights reserved.
+//
+
+import Foundation
+import XCTest
+import Elevate

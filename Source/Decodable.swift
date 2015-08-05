4d4f07781ae207e033cc008b87b29b48d85ec0b1
Eric Appel <eric.appel@nike.com>
2015-08-05 16:46:13 -0700

    docstring updates for Decodable.swift

--- Source/Decodable.swift
+++ Source/Decodable.swift
@@ -10,14 +10,36 @@ import Foundation
 
 // MARK: - Decodable Protocol Definition
 
-// TODO: Add docstring
+/**
+    The `Decodable` protocol declares an interface used to create an instance of an object from an `AnyObject` to be 
+    parsed.
+*/
 public protocol Decodable {
+    /**
+        Initializes an instance of the object using the given json object.
+    
+        - parameter json: The data to be parsed.
+    
+        - throws: ParserError.Validation or ParserError.Deserialization
+    */
     init(json: AnyObject) throws
 }
 
 // MARK: - Primative Decodables
 
+/**
+    The primative decodables implemented below are used by the parser when parsing an array of primative values. The 
+    input is expected to be of the same type as the object and will be validated and cast as such.
+*/
+
 extension String: Decodable {
+    /**
+        Implements the `Decodable` protocol for the `String` type. Expects input to be a `String`.
+    
+        - parameter json: A `String` instance
+        
+        - throws: ParserError.Validation
+    */
     public init(json: AnyObject) throws {
         guard Parser.valueIsSpecifiedType(value: json, type: .String) else {
             throw ParserError.Validation(failureReason: "JSON object was not of type: String")
@@ -28,6 +50,13 @@ extension String: Decodable {
 }
 
 extension Int: Decodable {
+    /**
+        Implements the `Decodable` protocol for the `Int` type. Expects input to be an `Int`.
+
+        - parameter json: An `Int` instance
+
+        - throws: ParserError.Validation
+    */
     public init(json: AnyObject) throws {
         guard Parser.valueIsSpecifiedType(value: json, type: .Int) else {
             throw ParserError.Validation(failureReason: "JSON object was not of type: Int")
@@ -38,6 +67,13 @@ extension Int: Decodable {
 }
 
 extension UInt: Decodable {
+    /**
+        Implements the `Decodable` protocol for the `UInt` type. Expects input to be a `UInt`.
+
+        - parameter json: A `UInt` instance
+
+        - throws: ParserError.Validation
+    */
     public init(json: AnyObject) throws {
         guard Parser.valueIsSpecifiedType(value: json, type: .UInt) else {
             throw ParserError.Validation(failureReason: "JSON object was not of type: UInt")
@@ -48,6 +84,13 @@ extension UInt: Decodable {
 }
 
 extension Float: Decodable {
+    /**
+        Implements the `Decodable` protocol for the `Float` type. Expects input to be a `Float`.
+
+        - parameter json: A `Float` instance
+
+        - throws: ParserError.Validation
+    */
     public init(json: AnyObject) throws {
         guard Parser.valueIsSpecifiedType(value: json, type: .Float) else {
             throw ParserError.Validation(failureReason: "JSON object was not of type: Float")
@@ -58,6 +101,13 @@ extension Float: Decodable {
 }
 
 extension Double: Decodable {
+    /**
+        Implements the `Decodable` protocol for the `Double` type. Expects input to be a `Double`.
+
+        - parameter json: A `Double` instance
+
+        - throws: ParserError.Validation
+    */
     public init(json: AnyObject) throws {
         guard Parser.valueIsSpecifiedType(value: json, type: .Double) else {
             throw ParserError.Validation(failureReason: "JSON object was not of type: Double")
@@ -68,6 +118,13 @@ extension Double: Decodable {
 }
 
 extension Bool: Decodable {
+    /**
+        Implements the `Decodable` protocol for the `Bool` type. Expects input to be a `Bool`.
+
+        - parameter json: A `Bool` instance
+
+        - throws: ParserError.Validation
+    */
     public init(json: AnyObject) throws {
         guard Parser.valueIsSpecifiedType(value: json, type: .Bool) else {
             throw ParserError.Validation(failureReason: "JSON object was not of type: Bool")

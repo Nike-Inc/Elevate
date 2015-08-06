9fc91b4123624ac39abeb215a8a952f104d3b1c8
Eric Appel <eric.appel@nike.com>
2015-08-05 21:41:41 -0700

    updated docstrings for ParserError.swift and ParserProperty.swift

--- Source/ParserError.swift
+++ Source/ParserError.swift
@@ -8,12 +8,17 @@
 
 import Foundation
 
-// TODO: Add docstring
+/**
+    The error types that can be thrown in Elevate
+
+    - Deserialization:  An error that occurs from deserializing using `NSJSONSerialization`.
+    - Validation:       An error that occurs when one or more values fail validation.
+*/
 public enum ParserError: ErrorType, CustomStringConvertible, CustomDebugStringConvertible {
     case Deserialization(failureReason: String)
     case Validation(failureReason: String)
 
-    // TODO: Add docstring
+    /// The failure reason String
     public var failureReason: String {
         switch self {
         case .Deserialization(let failureReason):
@@ -23,7 +28,7 @@ public enum ParserError: ErrorType, CustomStringConvertible, CustomDebugStringCo
         }
     }
 
-    // TODO: Add docstring
+    /// The description of the failure reason
     public var description: String {
         switch self {
         case .Deserialization(let failureReason):
@@ -33,7 +38,7 @@ public enum ParserError: ErrorType, CustomStringConvertible, CustomDebugStringCo
         }
     }
 
-    // TODO: Add docstring
+    /// The debug description of the failure reason
     public var debugDescription: String {
         return self.description
     }

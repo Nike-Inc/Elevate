1210c0e377d55e0912be058b3691b27d152332f4
Christian Noon <christian.noon@nike.com>
2016-06-01 14:59:17 -0700

    Fixed up license and copyright header to use Nike, Inc. and https URL.

--- Tests/ParserInternalTests.swift
+++ Tests/ParserInternalTests.swift
@@ -1,7 +1,7 @@
 //
 //  InternalParserTests.swift
 //
-//  Copyright (c) 2015-2016 Nike (http://www.nike.com)
+//  Copyright (c) 2015-2016 Nike, Inc. (https://www.nike.com)
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal

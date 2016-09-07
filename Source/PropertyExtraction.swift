9a7ad12d75804d9309f94cfcd0ff1f222faf06fe
Eric Appel <eric.appel@nike.com>
2016-09-07 15:45:18 -0700

    updated documentation comment format for swift 3

--- Source/PropertyExtraction.swift
+++ Source/PropertyExtraction.swift
@@ -35,51 +35,43 @@ infix operator <-? : DictionaryExtractionPrecedence
 infix operator <--! : DictionaryExtractionPrecedence
 infix operator <--? : DictionaryExtractionPrecedence
 
-/**
-    Forcibly extracts the value for the key from the dictionary as the specified type.
-
-    - parameter lhs: Dictionary to extract the value from.
-    - parameter rhs: Key of the value to extract from the dictionary.
-
-    - returns: Value for the key in the dictionary.
-*/
+/// Forcibly extracts the value for the key from the dictionary as the specified type.
+///
+/// - parameter lhs: Dictionary to extract the value from.
+/// - parameter rhs: Key of the value to extract from the dictionary.
+///
+/// - returns: Value for the key in the dictionary.
 public func <-! <T>(lhs: [String: Any], rhs: String) -> T {
     return lhs[rhs] as! T
 }
 
-/**
-    Safely extracts the value for the key from the dictionary as the specified optional type.
-
-    - parameter lhs: Dictionary to extract the value from.
-    - parameter rhs: Key of the value to extract from the dictionary.
-
-    - returns: Value for the key in the dictionary.
-*/
+/// Safely extracts the value for the key from the dictionary as the specified optional type.
+///
+/// - parameter lhs: Dictionary to extract the value from.
+/// - parameter rhs: Key of the value to extract from the dictionary.
+///
+/// - returns: Value for the key in the dictionary.
 public func <-? <T>(lhs: [String: Any], rhs: String) -> T? {
     return lhs[rhs] as? T
 }
 
-/**
-    Forcibly extracts the array for the key from the dictionary as the specified array type.
-
-    - parameter lhs: Dictionary to extract the array from.
-    - parameter rhs: Key of the array to extract from the dictionary.
-
-    - returns: Array for the key in the dictionary.
-*/
+/// Forcibly extracts the array for the key from the dictionary as the specified array type.
+///
+/// - parameter lhs: Dictionary to extract the array from.
+/// - parameter rhs: Key of the array to extract from the dictionary.
+///
+/// - returns: Array for the key in the dictionary.
 public func <--! <T>(lhs: [String: Any], rhs: String) -> [T] {
     guard let array = lhs[rhs] else { return [] }
     return array as! [T]
 }
 
-/**
-    Safely extracts the array for the key from the dictionary as the specified optional array type.
-
-    - parameter lhs: Dictionary to extract the array from.
-    - parameter rhs: Key of the array to extract from the dictionary.
-
-    - returns: Array for the key in the dictionary.
-*/
+/// Safely extracts the array for the key from the dictionary as the specified optional array type.
+///
+/// - parameter lhs: Dictionary to extract the array from.
+/// - parameter rhs: Key of the array to extract from the dictionary.
+///
+/// - returns: Array for the key in the dictionary.
 public func <--? <T>(lhs: [String: Any], rhs: String) -> [T]? {
     guard let array = lhs[rhs] else { return nil }
     return array as? [T]

255e8b2d806391f6fd332b4a1053faf06b0c6538
Dave Camp <Dave.Camp2@nike.com>
2017-02-09 12:54:36 -0800

    Re-added test that was accidentally deleted

--- Tests/PropertyExtractionTests.swift
+++ Tests/PropertyExtractionTests.swift
@@ -135,6 +135,18 @@ class PropertyExtractionTestCase: BaseTestCase {
         XCTAssertEqual(anyArray, ["value_0", "value_1"])
     }
 
+    func testOptionalArrayForKeyPathOperator() {
+        // Given, When
+        let stringsArray: [String]? = properties <--? "array"
+        let anyArray: [String]? = properties <--? "array_of_any_values"
+        let missingKey: [String]? = properties <--? "key_does_not_exist"
+
+        // Then
+        XCTAssertEqual(stringsArray ?? [], ["value_0", "value_1"])
+        XCTAssertEqual(anyArray ?? [], ["value_0", "value_1"])
+        XCTAssertNil(missingKey)
+    }
+
     func testExtractionPrecedence() {
         // Given, When
         let string: String = properties <-? "nope" ?? "default string"
